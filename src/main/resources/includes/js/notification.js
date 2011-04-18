e11ee3ad56197c96f98de56ec7268b7e4667d88c
Julien Hoarau <madgnome@gmail.com>
2011-04-18 19:15:20 +0200

    Notify user of new achievement. Closes #1

--- src/main/resources/includes/js/notification.js
+++ src/main/resources/includes/js/notification.js
@@ -1,21 +1,21 @@
-AJS.$(document).ready(function() {
-  AJS.$.getJSON(AJS.params.baseURL + '/rest/jirachievement/1.0/achievement/user', function(data) {
-    var items = [];
+(function($) {
+  $(document).ready(function() {
+    $.getJSON(AJS.params.baseURL + '/rest/jirachievement/1.0/achievement/user', function(data) {
+      var items = [];
 
-    AJS.$.each(data, function(key, val) {
-      items.push('<li id="' + key + '">' + val + '</li>');
+      $.each(data, function(){
+        var options = {
+          corners: '30px',
+          close: function(e,m,o)
+          {
+            // Change notified attribute to true
+          }
+        };
+        $.jGrowl(this.ref, options);
+      });
+
+            
     });
-
-    AJS.$('<ul/>', {
-      'class': 'my-new-list',
-      html: items.join('')
-    }).appendTo('body');
   });
-  /*var container = AJS.$('<div id="notify-container"/>');
-  var notification = AJS.$('<div id="notify-1"/>');
-  notification.append(AJS.$('<span class="notify-close"><a>x</a></span>'));
-  notification.append(AJS.$('<span>You earn a new badge : Necromancer</span>'));
-
-  container.append(notification);
-  AJS.$('body').prepend(container);*/
-});
+  
+})(jQuery);
