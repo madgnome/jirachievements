f0284862a25ece821622c8eb98906426d22d23ae
Julien Hoarau <madgnome@gmail.com>
2011-10-06 21:42:34 +0200

    JHERO-5 : Config initialization in upgradeTask v2

--- src/main/java/com/madgnome/jira/plugins/jirachievements/data/upgrades/v2/StatisticsUpgradeTask.java
+++ src/main/java/com/madgnome/jira/plugins/jirachievements/data/upgrades/v2/StatisticsUpgradeTask.java
@@ -1,11 +1,24 @@
 package com.madgnome.jira.plugins.jirachievements.data.upgrades.v2;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
+import com.atlassian.jira.config.ConstantsManager;
+import com.atlassian.jira.issue.status.Status;
+import com.madgnome.jira.plugins.jirachievements.data.ao.*;
+import com.madgnome.jira.plugins.jirachievements.data.services.IConfigDaoService;
+import com.madgnome.jira.plugins.jirachievements.data.services.impl.ConfigDaoService;
 import com.madgnome.jira.plugins.jirachievements.data.upgrades.AbstractUpgradeTask;
 import com.madgnome.jira.plugins.jirachievements.data.utils.KeyableUtils;
 
 public class StatisticsUpgradeTask extends AbstractUpgradeTask
 {
+  private IConfigDaoService configDaoService;
+  private final ConstantsManager constantsManager;
+
+  public StatisticsUpgradeTask(ConstantsManager constantsManager)
+  {
+    this.constantsManager = constantsManager;
+  }
+
   @Override
   protected int getVersion()
   {
@@ -16,11 +29,43 @@ public class StatisticsUpgradeTask extends AbstractUpgradeTask
   @SuppressWarnings({"unchecked"})
   protected void doUpgrade(ActiveObjects ao)
   {
-    ao.migrate(ComponentStatistic.class,
+    ao.migrate(Achievement.class,
+               ComponentStatistic.class,
+               Config.class,
+               Level.class,
                ProjectStatistic.class,
+               Statistic.class,
+               StatisticRef.class,
+               UserAchievement.class,
                UserStatistic.class,
+               UserWrapper.class,
                VersionStatistic.class);
 
+    configDaoService = new ConfigDaoService(ao);
+
+    upgradeConfig();
+    upgradeStatistics(ao);
+  }
+
+  private void upgradeConfig()
+  {
+    createConfigForStatus(ConfigRefEnum.OPEN_WORKFLOW_STATUSES, "Open");
+    createConfigForStatus(ConfigRefEnum.RESOLVED_WORKFLOW_STATUSES, "Resolved");
+    createConfigForStatus(ConfigRefEnum.REOPENED_WORKFLOW_STATUSES, "Reopened");
+    createConfigForStatus(ConfigRefEnum.CLOSED_WORKFLOW_STATUSES, "Closed");
+  }
+
+  private void createConfigForStatus(ConfigRefEnum configRef, String statusName)
+  {
+    Status status = constantsManager.getStatusByName(statusName);
+    if (status != null)
+    {
+      configDaoService.getOrCreate(configRef, status.getId());
+    }
+  }
+
+  private void upgradeStatistics(ActiveObjects ao)
+  {
     for (UserStatistic statistic : ao.find(UserStatistic.class))
     {
       statistic.setKey(KeyableUtils.buildKey(statistic));
--- src/main/java/com/madgnome/jira/plugins/jirachievements/utils/initializers/ConfigsInitializer.java
+++ src/main/java/com/madgnome/jira/plugins/jirachievements/utils/initializers/ConfigsInitializer.java
@@ -15,17 +15,7 @@ public class ConfigsInitializer implements ITableInitializer
   @Override
   public void initialize()
   {
-    configDaoService.getOrCreate(ConfigRefEnum.OPEN_WORKFLOW_STATUSES, "Open");
-    configDaoService.getOrCreate(ConfigRefEnum.RESOLVED_WORKFLOW_STATUSES, "Resolved");
-    configDaoService.getOrCreate(ConfigRefEnum.CLOSED_WORKFLOW_STATUSES, "Close");
-    configDaoService.getOrCreate(ConfigRefEnum.REOPENED_WORKFLOW_STATUSES, "Reopened");
-    
     configDaoService.getOrCreate(ConfigRefEnum.ACHIEVEMENTS_REFRESH_RATE, "60");
     configDaoService.getOrCreate(ConfigRefEnum.STATISTICS_REFRESH_RATE, Integer.toString(24*60));
-
-    configDaoService.getOrCreate(ConfigRefEnum.OPEN_WORKFLOW_STATUSES, "Open");
-    configDaoService.getOrCreate(ConfigRefEnum.RESOLVED_WORKFLOW_STATUSES, "Resolved");
-    configDaoService.getOrCreate(ConfigRefEnum.REOPENED_WORKFLOW_STATUSES, "Reopened");
-    configDaoService.getOrCreate(ConfigRefEnum.CLOSED_WORKFLOW_STATUSES, "Closed");
   }
 }
